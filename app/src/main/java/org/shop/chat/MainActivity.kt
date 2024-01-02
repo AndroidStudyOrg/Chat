4d7aada0de1f1ee79210bf4d57b5bd6b041e6ab5
wbham <wbham97@naver.com>
2024-01-02 14:57:51 +0900

    initialsetting

---
+++ app/src/main/java/org/shop/chat/MainActivity.kt
@@ -0,0 +1,17 @@
+package org.shop.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.shop.chat.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+
+    }
+}
